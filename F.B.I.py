c142e0fcd9b691a75458925c637d9abed2bf3a9c
Gabriella Cukier <you@example.com>
2019-05-31 19:04:37 -0300

    vidas

--- F.B.I.py
+++ F.B.I.py
@@ -666,7 +666,7 @@ def game_screen(screen, assets,level,score,submarino_img) :
     
     
     # Loop principal.
-    lives = 99
+    lives = 10
 
     
     background = fundo_nivel(config['fundo'])
